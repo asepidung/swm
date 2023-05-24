39ec47678fc69da1b2a768d43370f91706092f0e
unknown <asepsaidung@gmail.com>
2023-05-24 11:32:10 +0700

    menambahkan query option dan pembenahan for name

--- boning/boning.php
+++ boning/boning.php
@@ -17,24 +17,34 @@ include "kodebatchboning.php";
 <body>
   <form method="POST" action="inputboning.php">
     <label for="batchboning">BATCH</label>
-    <input type="text" name="batcboning" id="batchboning" value="<?= $kodeauto; ?>" readonly>
+    <input type="text" name="batchboning" id="batchboning" value="<?= $kodeauto; ?>" readonly>
     <br>
-    <label for="tglkilling">Tanggal Potong</label>
-    <input type="date" name="tglkilling" id="tglkilling">
+    <label for="tglkill">Tanggal Potong</label>
+    <input type="date" name="tglkill" id="tglkill">
     <br>
     <label for="tglboning">Tanggal Boning</label>
     <input type="date" name="tglboning" id="tglboning">
     <br>
-    <label for="pemasok">Supplier Sapi</label>
-    <select name="pemasok" id="pemasok">
+    <label for="idpemasok">Supplier Sapi</label>
+    <select name="idpemasok" id="idpemasok">
       <option value="">--Pilih Supplier Sapi</option>
-      <option value="">H. DONI</option>
+      <?php
+      $query = "SELECT * FROM pemasok";
+      $result = mysqli_query($conn, $query);
+
+      // Generate options based on the retrieved data
+      while ($row = mysqli_fetch_assoc($result)) {
+        $idpemasok = $row['idpemasok'];
+        $nmpemasok = $row['nmpemasok'];
+        echo "<option value=\"$idpemasok\">$nmpemasok</option>";
+      }
+      ?>
     </select>
     <br>
     <label for="qtysapi">Jumlah Sapi</label>
     <input type="number" name="qtysapi" id="qtysapi">
     <br>
-    <input type="submit" value="SUBMIT">
+    <input type="submit" value="submit">
   </form>
 
 </body>
