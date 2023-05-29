fb636607a7e0d2d49ebae5cb5b865920e1c65296
unknown <asepsaidung@gmail.com>
2023-05-29 22:49:18 +0700

    benerin label dikit

--- label/labelcontoh.php
+++ label/labelcontoh.php
@@ -1,45 +1,94 @@
-<table border="0" cellpadding="0" cellspacing="0" style="height:288px; width:384px">
+<table border="0" cellpadding="0" cellspacing="0" style="height: 288px; width: 384px">
   <tbody>
     <tr>
-      <td style="width:112px"><span style="font-size:14px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><strong>*YP*</strong></span></span></span></td>
-      <td style="width:149px">&nbsp;</td>
-      <td colspan="2" rowspan="1" style="text-align:right; width:114px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><strong>#01</strong></span></span></td>
-    </tr>
-    <tr>
-      <td colspan="4" style="width:264px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><strong>Prod By : PT. SANTI WIJAYA MEAT</strong></span></span></td>
-    </tr>
-    <tr>
-      <td colspan="4" style="white-space:nowrap; width:373px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">Jl. Perum Asabri Blok B No 20 Rt. 01/05 Ds. Sukasirna<br />
-            Kec. Jonggol Kab. Bogor</span></span></td>
-    </tr>
-    <tr>
-      <td colspan="2" rowspan="1" style="width:264px"><span style="font-size:22px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><strong>TENDERLOIN BUTT</strong></span></span></span></td>
-      <td colspan="2" rowspan="5" style="width:114px">&nbsp; &nbsp;<img alt="" src="https://ckeditor.com/apps/ckfinder/userfiles/files/download-removebg-preview.png" style="height:120px; width:68px" /></td>
-    </tr>
-    <tr>
-      <td colspan="1" rowspan="2" style="width:112px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><span style="font-size:24px"><strong>12.52</strong></span></span></span></td>
-      <td style="width:149px">
-        <p><strong><i>Tenderstreatch </i></strong></p>
+      <td style="width: 112px">
+        <span style="font-size: 14px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>*YP*</strong>
+        </span>
+      </td>
+      <td style="width: 149px">&nbsp;</td>
+      <td colspan="2" rowspan="1" style="text-align: right; width: 114px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>#01</strong>
+        </span>
       </td>
     </tr>
     <tr>
-      <td style="width:149px"><strong>3-Pcs</strong></td>
+      <td colspan="4" style="width: 264px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>Prod By: PT. SANTI WIJAYA MEAT</strong>
+        </span>
+      </td>
     </tr>
     <tr>
-      <td style="width:112px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">Packed Date&nbsp; :</span></span></td>
-      <td style="width:149px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">29-May-2023</span></span></td>
+      <td colspan="4" style="white-space: nowrap; width: 373px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          Jl. Perum Asabri Blok B No 20 Rt. 01/05 Ds. Sukasirna<br />
+          Kec. Jonggol Kab. Bogor
+        </span>
+      </td>
     </tr>
     <tr>
-      <td style="width:112px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">Expired Date :</span></span></td>
-      <td style="width:149px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">30-May-2023</span></span></td>
+      <td colspan="2" rowspan="1" style="width: 264px">
+        <span style="font-size: 22px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>TENDERLOIN BUTT</strong>
+        </span>
+      </td>
+      <td colspan="2" rowspan="5" style="width: 114px">
+        &nbsp; &nbsp;
+        <img alt="" src="https://ckeditor.com/apps/ckfinder/userfiles/files/download-removebg-preview.png" style="height: 120px; width: 68px" />
+      </td>
     </tr>
     <tr>
-      <td colspan="2" rowspan="1" style="width:264px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif"><strong>KEEP CHILL/FROZEN</strong></span></span></td>
-      <td colspan="2" rowspan="1" style="text-align:center; width:114px"><span style="font-size:11px"><span style="color:#000000"><span style="font-family:Tahoma,Geneva,sans-serif">No. 01011263450821<br />
-              NKV CS-3201170-027</span></span></span></td>
+      <td colspan="1" rowspan="2" style="width: 112px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <span style="font-size: 24px"><strong>12.52</strong></span>
+        </span>
+      </td>
+      <td style="width: 149px">
+        <p>
+          <strong><i>Tenderstreatch</i></strong>
+        </p>
+      </td>
     </tr>
     <tr>
-      <td colspan="4" style="text-align:center; width:373px"><span style="color:#000000"><span style="font-size:16px"><span style="font-family:Tahoma,Geneva,sans-serif">1230500100001</span></span></span></td>
+      <td style="width: 149px"><strong>3-Pcs</strong></td>
+    </tr>
+    <tr>
+      <td style="width: 112px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Packed Date&nbsp; :</span>
+      </td>
+      <td style="width: 149px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">29-May-2023</span>
+      </td>
+    </tr>
+    <tr>
+      <td style="width: 112px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Expired Date :</span>
+      </td>
+      <td style="width: 149px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">30-May-2023</span>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" rowspan="1" style="width: 264px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>KEEP CHILL/FROZEN</strong>
+        </span>
+      </td>
+      <td colspan="2" rowspan="1" style="text-align: center; width: 114px">
+        <span style="font-size: 11px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          No. 01011263450821<br />
+          NKV CS-3201170-027
+        </span>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="4" style="text-align: center; width: 373px">
+        <span style="color: #000000; font-size: 16px; font-family: Tahoma, Geneva, sans-serif">
+          1230500100001
+        </span>
+      </td>
     </tr>
   </tbody>
 </table>
