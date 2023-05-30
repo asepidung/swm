f8981f4f08e4b8870692ddc8fd18b4efe0fb7f57
unknown <asepsaidung@gmail.com>
2023-05-30 21:52:51 +0700

    desain label final

--- label/label.php
+++ label/label.php
@@ -1,93 +1,97 @@
 <?php
 require "../konak/conn.php";
-include "../assets/html/header.php";
-
+// include "../assets/html/header.php";
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-3">
-      <span style="font-size: 14px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <strong>*YP*</strong>
-      </span>
-    </div>
-    <div class="col-6"></div>
-    <div class="col-3 text-right">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <strong>#01</strong>
-      </span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <strong>Prod By: PT. SANTI WIJAYA MEAT</strong>
-      </span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        Jl. Perum Asabri Blok B No 20 Rt. 01/05 Ds. Sukasirna<br>Kec. Jonggol Kab. Bogor
-      </span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-8">
-      <span style="font-size: 22px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <strong>TENDERLOIN BUTT</strong>
-      </span>
-    </div>
-    <td colspan="2" rowspan="5" style="width: 114px" class="text-center">
-      <img src="../assets/dist/img/HalalIndonesia.png" alt="Halal Indonesia" width="80" height="120">
-    </td>
-  </div>
-  <div class="row">
-    <div class="col-4">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <span style="font-size: 24px">
-          <strong>12.52</strong>
+<table width="380" border="0" cellpadding="0" cellspacing="0">
+  <tbody>
+    <tr>
+      <td width="114" style="width: 112px">
+        <span style="font-size: 18px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>*YP*</strong>
         </span>
-      </span>
-    </div>
-    <div class="col-8">
-      <strong><i>Tenderstreatch</i></strong>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-4">
-      <strong>3-Pcs</strong>
-    </div>
-    <div class="col-8">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Packed Date: 29-May-2023</span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-4">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Expired Date:</span>
-    </div>
-    <div class="col-8">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">30-May-2023</span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-8">
-      <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        <strong>KEEP CHILL/FROZEN</strong>
-      </span>
-    </div>
-    <div class="col-4 text-center">
-      <span style="font-size: 11px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
-        No. 01011263450821<br>
-        NKV CS-3201170-027
-      </span>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12 text-center">
-      <span style="color: #000000; font-size: 16px; font-family: Tahoma, Geneva, sans-serif">
-        1230500100001
-      </span>
-    </div>
-  </div>
-</div>
+      </td>
+      <td width="149" style="width: 149px">&nbsp;</td>
+      <td width="121" colspan="2" rowspan="1" style="text-align: right; width: 114px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif; text-align: middle;">
+          <strong>#01</strong>
+        </span>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="4" style="width: 264px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>Prod By: PT. SANTI WIJAYA MEAT</strong>
+        </span>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="4" style="white-space: nowrap; width: 373px">
+        <p style="color: #000000; font-family: Tahoma, Geneva, sans-serif; font-size: 12px;">
+          Jl. Perum Asabri Blok B No 20 Rt. 01/05 Ds. Sukasirna<br>Kec. Jonggol Kab. Bogor
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td height="38" colspan="2" style="width: 264px">
+        <span style="font-size: 22px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>TENDERLOIN BUTT</strong>
+        </span>
+      </td>
+      <td colspan="2" rowspan="5" align="left"><img src="../assets/dist/img/hi.svg" alt="HALAL" width="70"></td>
+    </tr>
+    <tr>
+      <td colspan="1" rowspan="2" style="width: 112px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <span style="font-size: 24px"><strong>12.52</strong></span>
+        </span>
+      </td>
+      <td style="width: 149px">
+        <p>
+          <strong><i>3-Pcs</i></strong>
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td height="27" style="width: 149px; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, sans-serif; font-style: normal; font-size: 14px;"><strong>&#9733; <i>Tenderstreatch</i> &#x2605;</strong></td>
+    </tr>
+    <tr>
+      <td style="font-size: 12px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Packed Date&nbsp; :</span>
+      </td>
+      <td style="font-size: 12px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">29-May-2023</span>
+      </td>
+    </tr>
+    <tr>
+      <td height="17" style="font-size: 12px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Expired Date :</span>
+      </td>
+      <td style="font-size: 12px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">30-May-2023</span>
+      </td>
+    </tr>
+    <tr>
+      <td height="29" colspan="2" rowspan="1" style="width: 264px">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          <strong>KEEP CHILL/FROZEN</strong>
+        </span>
+      </td>
+      <td colspan="2" rowspan="1" style="text-align: left; width: 114px">
+        <span style="font-size: 11px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+          No. 01011263450821<br />
+          NKV CS-3201170-027
+        </span>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="4" style="text-align: center; width: 373px"><span style="color: #000000; font-size: 36px; font-family: '3 of 9 Barcode'">*1230500100001*</span></td>
+    </tr>
+    <tr>
+      <td colspan="4" style="text-align: center; width: 373px">
+        <span style="color: #000000; font-size: 18px; font-family: Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif">
+          1230500100001</span>
+      </td>
+    </tr>
+  </tbody>
+</table>
--- label/labelcontoh.php
+++ label/labelcontoh.php
@@ -7,13 +7,13 @@ require "../konak/conn.php";
   <tbody>
     <tr>
       <td width="114" style="width: 112px">
-        <span style="font-size: 14px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
+        <span style="font-size: 18px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
           <strong>*YP*</strong>
         </span>
       </td>
       <td width="149" style="width: 149px">&nbsp;</td>
       <td width="121" colspan="2" rowspan="1" style="text-align: right; width: 114px">
-        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
+        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif; text-align: middle;">
           <strong>#01</strong>
         </span>
       </td>
@@ -27,19 +27,17 @@ require "../konak/conn.php";
     </tr>
     <tr>
       <td colspan="4" style="white-space: nowrap; width: 373px">
-        <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
-          Jl. Perum Asabri Blok B No 20 Rt. 01/05<br> Ds. Sukasirna
-          Kec. Jonggol Kab. Bogor
-        </span>
+        <p style="color: #000000; font-family: Tahoma, Geneva, sans-serif; font-size: 12px;">
+          Jl. Perum Asabri Blok B No 20 Rt. 01/05 Ds. Sukasirna<br>Kec. Jonggol Kab. Bogor
+        </p>
       </td>
     </tr>
     <tr>
-      <td colspan="2" rowspan="1" style="width: 264px">
+      <td height="38" colspan="2" style="width: 264px">
         <span style="font-size: 22px; color: #000000; font-family: Tahoma, Geneva, sans-serif">
           <strong>TENDERLOIN BUTT</strong>
-        </span>
-      </td>
-      <td colspan="2" rowspan="5"><img src="../assets/dist/img/HalalIndo.jpg" alt="HALAL" width="77" height="120" align="left"></td>
+        </span>      </td>
+      <td colspan="2" rowspan="5" align="left"><img src="../assets/dist/img/hi.svg" alt="HALAL" width="70"></td>
     </tr>
     <tr>
       <td colspan="1" rowspan="2" style="width: 112px">
@@ -54,26 +52,26 @@ require "../konak/conn.php";
       </td>
     </tr>
     <tr>
-      <td style="width: 149px"><strong><i>Tenderstreatch</i></strong></td>
+		<td height="27" style="width: 149px; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, sans-serif; font-style: normal; font-size: 14px;"><strong>&#9733; <i>Tenderstreatch</i> &#x2605;</strong></td>
     </tr>
     <tr>
-      <td style="width: 112px">
+      <td style="font-size: 12px">
         <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Packed Date&nbsp; :</span>
       </td>
-      <td style="width: 149px">
+      <td style="font-size: 12px">
         <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">29-May-2023</span>
       </td>
     </tr>
     <tr>
-      <td height="29" style="width: 112px">
+      <td height="17" style="font-size: 12px">
         <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">Expired Date :</span>
       </td>
-      <td style="width: 149px">
+      <td style="font-size: 12px">
         <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">30-May-2023</span>
       </td>
     </tr>
     <tr>
-      <td colspan="2" rowspan="1" style="width: 264px">
+      <td height="29" colspan="2" rowspan="1" style="width: 264px">
         <span style="color: #000000; font-family: Tahoma, Geneva, sans-serif">
           <strong>KEEP CHILL/FROZEN</strong>
         </span>
@@ -86,10 +84,12 @@ require "../konak/conn.php";
       </td>
     </tr>
     <tr>
+      <td colspan="4" style="text-align: center; width: 373px"><span style="color: #000000; font-size: 36px; font-family: '3 of 9 Barcode'">*1230500100001*</span></td>
+    </tr>
+    <tr>
       <td colspan="4" style="text-align: center; width: 373px">
-        <span style="color: #000000; font-size: 16px; font-family: Tahoma, Geneva, sans-serif">
-          1230500100001
-        </span>
+        <span style="color: #000000; font-size: 18px; font-family: Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif">
+          1230500100001</span>
       </td>
     </tr>
   </tbody>
