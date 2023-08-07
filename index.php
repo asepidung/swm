1adc4e4906939c5bc18409cbb00f85576b144d8c
unknown <asepsaidun@gmail.com>
2023-08-07 08:34:23 +0700

    perbaikan index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['login'])) {
-   header("location: ../verifications/login.php");
+   header("location: verifications/login.php");
 }
 require "konak/conn.php";
 
