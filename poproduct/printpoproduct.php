f478bc925762af972eaee47a843cd8a22da5c316
unknown <asepsaidung@gmail.com>
2023-09-05 15:21:38 +0700

    title po print

--- poproduct/printpoproduct.php
+++ poproduct/printpoproduct.php
@@ -29,7 +29,7 @@ $result_poproductdetail = mysqli_query($conn, $query_poproductdetail);
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="widtd=device-widtd, initial-scale=1">
-   <title>Cetak PO</title>
+   <title><?= $row_poproduct['nopoproduct'] . " " . $row_poproduct['nmsupplier']; ?></title>
    <link rel="icon" href="../dist/img/favicon.png" type="image/x-icon">
    <link rel="stylesheet" href="../https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
