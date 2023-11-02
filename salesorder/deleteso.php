a0d3c7b935943be5d0726508d86e469a0966db67
unknown <asepsaidung@gmail.com>
2023-11-03 00:46:27 +0700

    foreign so

--- salesorder/deleteso.php
+++ salesorder/deleteso.php
@@ -8,14 +8,21 @@ require "../konak/conn.php";
 if (isset($_GET['idso'])) {
    $idsalesorder = $_GET['idso'];
 
-   // Delete data dari tabel salesorderdetail
+   // Hapus data dari tabel plandev terlebih dahulu
+   $query_delete_plandev = "DELETE FROM plandev WHERE idso = ?";
+   $stmt_delete_plandev = $conn->prepare($query_delete_plandev);
+   $stmt_delete_plandev->bind_param("i", $idsalesorder);
+   $stmt_delete_plandev->execute();
+   $stmt_delete_plandev->close();
+
+   // Hapus data dari tabel salesorderdetail
    $query_delete_salesorderdetail = "DELETE FROM salesorderdetail WHERE idso = ?";
    $stmt_delete_salesorderdetail = $conn->prepare($query_delete_salesorderdetail);
    $stmt_delete_salesorderdetail->bind_param("i", $idsalesorder);
    $stmt_delete_salesorderdetail->execute();
    $stmt_delete_salesorderdetail->close();
 
-   // Delete data dari tabel salesorder
+   // Hapus data dari tabel salesorder
    $query_delete_salesorder = "DELETE FROM salesorder WHERE idso = ?";
    $stmt_delete_salesorder = $conn->prepare($query_delete_salesorder);
    $stmt_delete_salesorder->bind_param("i", $idsalesorder);
