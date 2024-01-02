afc877f2b6ee766d2907a6b0a28ba1e07ca00789
unknown <asepsaidung@gmail.com>
2024-01-02 16:00:47 +0700

    selesai hari ini

--- tally/inputtallydetail.php
+++ tally/inputtallydetail.php
@@ -6,46 +6,58 @@ if (!isset($_SESSION['login'])) {
 require "../konak/conn.php";
 if (isset($_POST['barcode'])) {
    $barcode = $_POST['barcode'];
-   $idrepack = $_POST['idrepack'];
+   $idtally = $_POST['idtally'];
+   $idso_query = "SELECT idso FROM tally WHERE idtally = $idtally";
+   $idso_result = mysqli_query($conn, $idso_query);
+
+   if ($idso_result) {
+      $idso_row = mysqli_fetch_assoc($idso_result);
+      $idso = $idso_row['idso'];
+   }
 
    // Langsung query ke tabel stock berdasarkan barcode
-   $query = "SELECT idbarang, idgrade, qty, pcs, pod, origin FROM stock WHERE kdbarcode = '$barcode'";
+   $query = "SELECT idbarang, qty, pcs, pod, origin FROM stock WHERE kdbarcode = '$barcode'";
 
    // Eksekusi query
    $result = mysqli_query($conn, $query);
 
    if ($result && $row = mysqli_fetch_assoc($result)) {
       $idbarang = $row['idbarang'];
-      $idgrade = $row['idgrade'];
-      $qty = $row['qty']; // Menyesuaikan nama kolom di tabel
+      $weight = $row['qty']; // Menyesuaikan nama kolom di tabel
       $pcs = $row['pcs'];
       $pod = $row['pod'];
       $origin = $row['origin'];
 
-      // Selanjutnya, kita akan melakukan pengecekan apakah $barcode sudah ada di tabel detailbahan
-      $cekBarcodeQuery = "SELECT iddetailbahan FROM detailbahan WHERE idrepack = $idrepack AND barcode = '$barcode'";
-      $cekBarcodeResult = mysqli_query($conn, $cekBarcodeQuery);
+      // Pengecekan apakah idbarang ada dalam salesorderdetail
+      $cekBarangQuery = "SELECT idbarang FROM salesorderdetail WHERE idso = $idso AND idbarang = $idbarang";
+      $cekBarangResult = mysqli_query($conn, $cekBarangQuery);
 
-      if (mysqli_num_rows($cekBarcodeResult) > 0) {
-         // Barcode sudah ada di tabel detailbahan, arahkan kembali ke halaman dengan status "duplicate"
-         header("location: detailbahan.php?id=$idrepack&stat=duplicate");
+      if (mysqli_num_rows($cekBarangResult) == 0) {
+         // Barang tidak ada dalam salesorderdetail, arahkan kembali ke halaman unlisted
+         header("location: tallydetail.php?id=$idtally&stat=unlisted");
          exit;
       } else {
-         // Barcode belum ada di tabel detailbahan, lanjutkan dengan query insert
-         $insertQuery = "INSERT INTO detailbahan (idrepack, barcode, idbarang, idgrade, qty, pcs, pod, origin) VALUES ('$idrepack', '$barcode', '$idbarang',  '$idgrade', '$qty', '$pcs', '$pod', '$origin')";
-         mysqli_query($conn, $insertQuery);
+         // Selanjutnya, kita akan melakukan pengecekan apakah $barcode sudah ada di tabel tallydetail
+         $cekBarcodeQuery = "SELECT idtallydetail FROM tallydetail WHERE idtally = $idtally AND barcode = '$barcode'";
+         $cekBarcodeResult = mysqli_query($conn, $cekBarcodeQuery);
 
-         // Hapus data dari tabel stock
-         $deleteQuery = "DELETE FROM stock WHERE kdbarcode = '$barcode'";
-         mysqli_query($conn, $deleteQuery);
+         if (mysqli_num_rows($cekBarcodeResult) > 0) {
+            // Barcode sudah ada di tabel tallydetail, arahkan kembali ke halaman dengan status "duplicate"
+            header("location: tallydetail.php?id=$idtally&stat=duplicate");
+            exit;
+         } else {
+            // Barcode belum ada di tabel tallydetail, lanjutkan dengan query insert
+            $insertQuery = "INSERT INTO tallydetail (idtally, barcode, idbarang, weight, pcs, pod, origin) VALUES ('$idtally', '$barcode', '$idbarang', '$weight', '$pcs', '$pod', '$origin')";
+            mysqli_query($conn, $insertQuery);
 
-         // Redirect kembali ke halaman detailbahan.php dengan status "success"
-         header("location: detailbahan.php?id=$idrepack&stat=success");
+            // Redirect kembali ke halaman tallydetail.php dengan status "success"
+            header("location: tallydetail.php?id=$idtally&stat=success");
+         }
       }
    } else {
       // Barcode tidak ditemukan di tabel stock
       $_SESSION['barcode'] = $barcode;
-      header("location: detailbahan.php?id=$idrepack&stat=unknown");
+      header("location: tallydetail.php?id=$idtally&stat=unknown");
       exit;
    }
 }
