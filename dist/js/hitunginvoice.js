ff3ed482abde44236ab2a8ae36b7fc48221a374d
unknown <asepsaidung@gmail.com>
2024-05-03 15:39:06 +0700

    invoice sudah bisa dilakukan disemua devices

--- dist/js/hitunginvoice.js
+++ dist/js/hitunginvoice.js
@@ -82,7 +82,7 @@ function calculateAmounts() {
    var balance = totalAmount + taxAmount + charge - downPayment;
 
    // Update the relevant fields with the calculated values
-   document.getElementById('xamount').value = parseFloat(totalAmount.toFixed(2)).toLocaleString(undefined, {
+   document.getElementById('xamount').value = parseFloat(totalAmount.toFixed(2)).toLocaleString('en-US', {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
    });
@@ -92,7 +92,7 @@ function calculateAmounts() {
 
    // Mengubah format discountrp[]
    for (var i = 0; i < discountrp.length; i++) {
-      discountrp[i].value = parseFloat(discountrp[i].value).toLocaleString(undefined, {
+      discountrp[i].value = parseFloat(discountrp[i].value).toLocaleString('en-US', {
          minimumFractionDigits: 2,
          maximumFractionDigits: 2
       });
@@ -100,7 +100,7 @@ function calculateAmounts() {
 
    // Mengubah format amounts[]
    for (var i = 0; i < amounts.length; i++) {
-      amounts[i].value = parseFloat(amounts[i].value).toLocaleString(undefined, {
+      amounts[i].value = parseFloat(amounts[i].value).toLocaleString('en-US', {
          minimumFractionDigits: 2,
          maximumFractionDigits: 2
       });
@@ -110,13 +110,13 @@ function calculateAmounts() {
    taxElement.value = taxAmount;
 
    // Mengubah format balance
-   balanceElement.value = parseFloat(balance.toFixed(2)).toLocaleString(undefined, {
+   balanceElement.value = parseFloat(balance.toFixed(2)).toLocaleString('en-US', {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
    });
 
    // Mengubah format xdiscount
-   document.getElementById('xdiscount').value = parseFloat(totalDiscount.toFixed(2)).toLocaleString(undefined, {
+   document.getElementById('xdiscount').value = parseFloat(totalDiscount.toFixed(2)).toLocaleString('en-US', {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
    });
--- inv/prosesinvoice.php
+++ inv/prosesinvoice.php
@@ -18,13 +18,28 @@ if (isset($_POST['submit'])) {
    $note = $_POST['note'];
    $pajak = $_POST['pajak'];
 
-   $xweight = $_POST['xweight'];
-   $xamount = str_replace(',', '', $_POST['xamount']);
-   $xdiscount = str_replace(',', '', $_POST['xdiscount']);
-   $tax = str_replace(',', '', $_POST['tax']);
-   $charge = str_replace(',', '', $_POST['charge']);
-   $downpayment = str_replace(',', '', $_POST['downpayment']);
-   $balance = str_replace(',', '', $_POST['balance']);
+   // Fungsi untuk mengubah format angka sesuai dengan pengaturan lokal
+   function formatNumber($number)
+   {
+      // Mengganti titik dengan koma dan sebaliknya, tergantung pengaturan lokal
+      $locale = localeconv();
+      $decimalPoint = $locale['decimal_point'];
+      $thousandsSep = $locale['thousands_sep'];
+      $number = str_replace($thousandsSep, '|', $number);
+      $number = str_replace($decimalPoint, $thousandsSep, $number);
+      $number = str_replace('|', $decimalPoint, $number);
+      return $number;
+   }
+
+   // Format angka sesuai pengaturan lokal
+   $xweight = formatNumber($_POST['xweight']);
+   $xamount = formatNumber($_POST['xamount']);
+   $xdiscount = formatNumber($_POST['xdiscount']);
+   $tax = formatNumber($_POST['tax']);
+   $charge = formatNumber($_POST['charge']);
+   $downpayment = formatNumber($_POST['downpayment']);
+   $balance = formatNumber($_POST['balance']);
+
    $tukarfaktur = $_POST['tukarfaktur'];
    // hitung duedate
    $invoice_date_obj = new DateTime($invoice_date);
@@ -66,10 +81,10 @@ if (isset($_POST['submit'])) {
    for ($i = 0; $i < count($idbarang); $i++) {
       $idbarang[$i] = $idbarang[$i];
       $weight[$i] = $weight[$i];
-      $price[$i] = str_replace(',', '', $price[$i]);
+      $price[$i] = formatNumber($price[$i]);
       $discount[$i] = $discount[$i];
-      $discountrp[$i] = str_replace(',', '', $discountrp[$i]);
-      $amount[$i] = str_replace(',', '', $amount[$i]);
+      $discountrp[$i] = formatNumber($discountrp[$i]);
+      $amount[$i] = formatNumber($amount[$i]);
 
       $sql = "INSERT INTO invoicedetail (idinvoice, idbarang, weight, price, discount, discountrp, amount) 
               VALUES ('$invoiceID', '{$idbarang[$i]}', '{$weight[$i]}', '{$price[$i]}', '{$discount[$i]}', '{$discountrp[$i]}', '{$amount[$i]}')";
